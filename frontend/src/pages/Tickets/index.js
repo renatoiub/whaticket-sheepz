7def814b2b9c89d7adc1e70504bcdf0442ef70ad
alison-nunes <alison.nunes@gmail.com>
2024-04-04 18:53:29 -0300

    Update index.js

--- frontend/src/pages/Tickets/index.js
+++ frontend/src/pages/Tickets/index.js
@@ -7,7 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import TicketsManager from "../../components/TicketsManager/";
 import Ticket from "../../components/Ticket/";
 
-import logo from "../../assets/logo.png";
+import logo from "../../assets/sheepz-icon.png";
 
 import { i18n } from "../../translate/i18n";
 
